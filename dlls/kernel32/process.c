9e57e32c613de16b930778c072ddd7ada4903151
Andrew Wesie <awesie@gmail.com>
2017-02-05 04:17:22 -0600

    ntdll: Read entry point in LdrInitializeThunk.
    Signed-off-by: Andrew Wesie <awesie@gmail.com>

--- dlls/kernel32/process.c
+++ dlls/kernel32/process.c
@@ -89,7 +89,6 @@ static const BOOL is_win64 = (sizeof(void *) > sizeof(int));
 
 HMODULE kernel32_handle = 0;
 SYSTEM_BASIC_INFORMATION system_info = { 0 };
-LPTHREAD_START_ROUTINE entry_proc = NULL;
 
 const WCHAR *DIR_Windows = NULL;
 const WCHAR *DIR_System = NULL;
@@ -1102,11 +1101,12 @@ static inline DWORD call_process_entry( PEB *peb, LPTHREAD_START_ROUTINE entry )
  *
  * Startup routine of a new process. Runs on the new process stack.
  */
-static DWORD WINAPI start_process( PEB *peb )
+static DWORD WINAPI start_process( LPTHREAD_START_ROUTINE entry )
 {
     BOOL being_debugged;
 
-    if (entry_proc == (LPTHREAD_START_ROUTINE)peb->ImageBaseAddress)
+    PEB *peb = NtCurrentTeb()->Peb;
+    if (!entry)
     {
         ERR( "%s doesn't have an entry point, it cannot be executed\n",
              debugstr_w(peb->ProcessParameters->ImagePathName.Buffer) );
@@ -1115,7 +1115,7 @@ static DWORD WINAPI start_process( PEB *peb )
 
     if (TRACE_ON(relay))
         DPRINTF( "%04x:Starting process %s (entryproc=%p)\n", GetCurrentThreadId(),
-                 debugstr_w(peb->ProcessParameters->ImagePathName.Buffer), entry_proc );
+                 debugstr_w(peb->ProcessParameters->ImagePathName.Buffer), entry );
 
     if (CreateEventA(0, 0, 0, "__winestaging_warn_event") && GetLastError() != ERROR_ALREADY_EXISTS)
     {
@@ -1130,7 +1130,7 @@ static DWORD WINAPI start_process( PEB *peb )
 
     SetLastError( 0 );  /* clear error code */
     if (being_debugged) DbgBreakPoint();
-    return call_process_entry( peb, entry_proc );
+    return call_process_entry( peb, entry );
 }
 
 
@@ -1211,7 +1211,6 @@ void CDECL __wine_kernel_init(void)
     RTL_USER_PROCESS_PARAMETERS *params = peb->ProcessParameters;
     HANDLE boot_events[2];
     BOOL got_environment = TRUE;
-    IMAGE_NT_HEADERS *nt;
 
     /* Initialize everything */
 
@@ -1324,10 +1323,6 @@ void CDECL __wine_kernel_init(void)
     }
 
     if (!params->CurrentDirectory.Handle) chdir("/"); /* avoid locking removable devices */
-
-    nt = RtlImageNtHeader( peb->ImageBaseAddress );
-    entry_proc = (LPTHREAD_START_ROUTINE)((char *)peb->ImageBaseAddress +
-                                          nt->OptionalHeader.AddressOfEntryPoint);
 
     LdrInitializeThunk( start_process, 0, 0, 0 );
 
